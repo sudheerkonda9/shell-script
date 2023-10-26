194ecfc74968cd9c5f1a06733d3253d31562e336
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 15:05:19 -0400

    Previos command status

--- 10-special-Variables.sh
+++ 10-special-Variables.sh
@@ -3,6 +3,8 @@
 VAR1=$1
 VAR2=$2
 
+echo "variable 1 is = $VAR1"
+echo "Variable 2 is = $VAR2"
 echo "Printing script name on terminal $0"
 echo "Printing Total number of variables passed = $#"
 echo "Print variable names: $@"
