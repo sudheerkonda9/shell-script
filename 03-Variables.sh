01c5a2e138be2dfba0e8066b7b11781bfd7f2a05
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 01:03:52 -0400

    Time stamp Printing

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello world , I am learning Shell script"
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, Very Good Morning"
+echo "$PERSON1: Hi $PERSON2, How r you"
+echo "$PERSON2: I am good, How about you $PERSON1"
