09772301270aa1c23ba74890d4d9c76f1c1fb0a4
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 00:30:08 -0400

    Used Variables

--- README2.sh
+++ README2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Hi Ramesh, Very Good Morning"
-echo "Ramesh: Hi Suresh, How r you"
-echo "Suresh: I am good, How about you Ramesh"
+echo "Hello world , I am learning Shell script"
+PERSON1=Ramesh
+PERSON2=SURESH
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, Very Good Morning"
+echo "$PERSON1: Hi $PERSON2, How r you"
+echo "$PERSON2: I am good, How about you $PERSON1"
