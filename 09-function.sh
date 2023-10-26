c4bafa83b61bc6c9781d085aa89aa2a901372501
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 15:56:00 -0400

    Previos command status

--- 09-function.sh
+++ 09-function.sh
@@ -11,10 +11,10 @@ N="\e[0m"
 VALIDATE () {
 if [ $1 -ne 0 ]
 then
-echo "$2....... $R FAILED $N"
+echo -e "$2....... $R FAILED $N"
 exit 1
 else
-echo "$2....... $G SUCCESS $N"
+echo -e "$2....... $G SUCCESS $N"
 fi
 }
 
