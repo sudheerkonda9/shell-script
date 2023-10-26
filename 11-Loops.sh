4e9a93b97e998eb96cce9172db283b31f83e11bb
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 16:29:02 -0400

    Previos command status

---
+++ 11-Loops.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# echo 1
+# echo 2
+# echo 3
+# echo 4
+# echo 5
+# .
+# .
+# .
+# echo 100
+
+
+# for Syntax : 
+# for variable in list
+# do
+# write your statement
+# done
+
+for i in {1..100}
+do 
+ echo $i
+ done
