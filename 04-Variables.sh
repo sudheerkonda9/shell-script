e07f695d9e17cbf7d8ba1c6bebaa0e9a6a83cc94
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 01:21:41 -0400

    Addition

---
+++ 04-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Add two numbers and print out put , two mumbers should be provided from output console
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$((NUMBER1+NUMBER2))
+
+echo "Addition ouput of two numbers $NUMBER1 and $NUMBER2 is ="
