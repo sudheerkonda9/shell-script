0459b1f0d37bf89e186c01d52a432c963a52a38b
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 01:44:57 -0400

    Addition

---
+++ 05-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Pleassss enter your username"
+read -s USERNAME
+
+echo "User Name is $USERNAME"
+echo "Pleassss enter your userPasswordname"
+read -s PASSWORD
+
+echo "Password is $PASSWORD"
