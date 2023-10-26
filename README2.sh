ce49d74648bc36b157e018ed1f306a54d04019e3
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 00:09:24 -0400

    Rmesh Suresh Script

--- README2.sh
+++ README2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "I am learning Shell Script and GIT REPO"
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Hi Ramesh, Very Good Morning"
+echo "Ramesh: Hi Suresh, How r you"
+echo "Suresh: I am good, How about you Ramesh"
