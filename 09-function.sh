fa3efaa75dc1798de722b6974e337a97e5c53477
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 15:54:00 -0400

    Previos command status

--- 09-function.sh
+++ 09-function.sh
@@ -4,13 +4,17 @@ DATE=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE () {
 if [ $1 -ne 0 ]
 then
-echo "$2.......FAILED"
+echo "$2....... $R FAILED $N"
 exit 1
 else
-echo "$2.......SUCCESS"
+echo "$2....... $G SUCCESS $N"
 fi
 }
 
@@ -30,7 +34,7 @@ yum install mysql -y &>>$LOGFILE
 VALIDATE $? "MYSQL Installation"
 
 # Post fix installation
-yum install postfix -y &>>$LOGFILE
+yum installdf postfix -y &>>$LOGFILE
 
 VALIDATE $? "POSTFIX Installation"
 
