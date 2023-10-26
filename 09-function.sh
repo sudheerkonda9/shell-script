2920c713591166480152476b45420627b3e4ae3a
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 15:22:54 -0400

    Previos command status

--- 09-function.sh
+++ 09-function.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-USERID=$(id -u)
-#This function should validate the previos command and inform user about Pass/fail
 
-if [ $USERID -ne 0 ]
-then 
-echo "ERROR:: $USERID is not a root user"
-exit 1
- else
-  echo "$USERID is a root user"
- fi
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
- VALIDATE () {
-
+VALIDATE () {
 if [ $1 -ne 0 ]
 then
 echo "$2.......FAILED"
@@ -20,13 +13,24 @@ else
 echo "$2.......SUCCESS"
 fi
 }
+
+USERID=$(id -u)
+#This function should validate the previos command and inform user about Pass/fail
+if [ $USERID -ne 0 ]
+then 
+echo "ERROR:: $USERID is not a root user"
+exit 1
+ else
+  echo "$USERID is a root user"
+ fi
+
  #My SQL installation
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 
 VALIDATE $? "MYSQL Installation"
 
 # Post fix installation
-yum instals postfix -y
+yum instals postfix -y &>>$LOGFILE
 
 VALIDATE $? "POSTFIX Installation"
 
