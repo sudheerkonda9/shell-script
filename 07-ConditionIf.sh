55ba213fd269c1538b4c9eb95daf63964e5a9b7b
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-26 10:42:55 -0400

    Array

--- 07-ConditionIf.sh
+++ 07-ConditionIf.sh
@@ -4,8 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-echo "$NUMBER is greater than 10"
-
+    echo "$NUMBER is greater than 10"
 else 
-
-echo "$NUMBER is less than 10"
+    echo "$NUMBER is less than 10"
+fi
