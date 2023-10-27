821dc65fb2c71a5c9859ded21102b4a63dc2357d
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-27 00:55:37 -0400

    Previos command status

--- 13-Loops.sh
+++ 13-Loops.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+DATE=$(date +%F)
+SCRIPTNAME=$0
+LOGSPATH=/home/centos/shellscript-logs
+LOGFILE=$LOGSPATH/$SCRIPTNAME-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "ERROR::$2 installation $R FAILED $N"
+    exit
+    else
+    echo -e "SUCCESS::$2 installation $G PASSED $N"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -17,5 +32,14 @@ fi
 
 for i in $@
 do
-    yum install $i -y
+    yum list installed $i &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+   echo "$i Package is not installed, Lets install $i package"
+   yum install $i -y &>>$LOGFILE
+   VALIDATE $? "$i"
+   else
+   echo -e "$Y $i Package already installed $N"
+fi
+
 done
