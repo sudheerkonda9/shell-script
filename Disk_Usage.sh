2194916cb36fe3e25c1d3ba0b638eb7d8f7e41f7
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-28 02:02:52 -0400

    Previos command status

--- Disk_Usage.sh
+++ Disk_Usage.sh
@@ -12,11 +12,24 @@ N="\e[0m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
+message=""
 
 
 #"IFS= " Internal field seperator is space
 while IFS= read line
 do 
- #Write your statements here
-  echo "output $line"
+#df -hT | grep -vE 'tmpfs|Filesystem' | awk '{print $6}' | cut -d% -f1
+#below command will give you usage in number format
+ usage=$(echo $line | awk '{print $6}' | cut -d% -f1 )
+ # I need partition also , below command will give us partition
+ partition=$(echo $line | awk '{print $1}')
+
+#now you need to check whther it is more than threshold or not
+if [$usage -gt $DISK_USAGE_THRESHOLD]
+ then
+ #below message + means add to the existing message. 
+   message+="HIGH DISK USAGE ON $partition: $usage"
+   fi
  done <<< $DISK_USAGE
+
+ echo "messge : $message" 
